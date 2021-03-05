2f156e8ef1c29a483280d0370769ead1f9532540
Tran V Hiep <tvhiep264@gmail.com>
2021-03-05 21:15:23 +0700

    bai tap array

--- Learn/js/main.js
+++ Learn/js/main.js
@@ -10,7 +10,6 @@ var testIndex1 = mang.indexOf('Cuoi');
 var testIndex2 = mang.lastIndexOf('Cuoi');
 
 console.log(mang);
-console.log(mang.sort());
 //In ra phan tu thu ... cua mang
 console.log(mang[4]);
 
@@ -20,4 +19,27 @@ console.log(mang.length);
 console.log(testIndexSo);
 console.log(testIndex1);
 console.log(testIndex2);
+
+console.log(mang.sort());
+console.log(mang.reverse()); 
+
+// Dùng for để duyệt theo index trong mảng
+/* for(var i=0; i< mang.length; i++){
+    console.log(mang[i]);
+} */
+
+// Dùng for of để duyệt theo index trong mảng (Nên dùng)
+//Dung for (nói chung) khi ĐÃ BIẾT số vòng lặp
+for (var ptu of mang){
+    console.log(ptu);
+}
+
+// Dung while khi KHÔNG BIẾT số vòng lặp
+/* var i = 0;
+while(i < mang.length){
+    console.log(mang);
+    i++;
+} */
+
+
 
